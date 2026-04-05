e87c0fd577e518c00f5e164b1a61c49b886eb6cc
NivzFPV <niv.tuki@gmail.com>
2026-04-05 12:16:11 +0300

    Configure build output to public_html and include built files

--- vite.config.js
+++ vite.config.js
@@ -2,4 +2,8 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   base: './', // This ensures all paths are relative, so it works on GitHub AND HostGator
+  build: {
+    outDir: 'public_html',
+    emptyOutDir: true,
+  }
 })
